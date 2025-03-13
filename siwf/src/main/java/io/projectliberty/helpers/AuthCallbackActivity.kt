414957661cea35e93b20c3684d09087b133623ab
Claire Olmstead <olmsteadclaire@gmail.com>
2025-03-13 13:23:17 -0700

    update intent name

--- siwf/src/main/java/io/projectliberty/helpers/AuthCallbackActivity.kt
+++ siwf/src/main/java/io/projectliberty/helpers/AuthCallbackActivity.kt
@@ -26,7 +26,7 @@ class AuthCallbackActivity : ComponentActivity() {
             if (authCode != null) {
                 Log.d("AuthCallback", "Auth Code: $authCode")
 
-                val broadcastIntent = Intent("com.example.siwf.AUTH_RESULT").apply {
+                val broadcastIntent = Intent("io.projectliberty.helpers.AUTH_RESULT").apply {
                     setPackage(packageName)
                     putExtra("authorizationCode", authCode)
                 }
